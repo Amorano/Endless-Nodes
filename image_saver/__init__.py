53b40394a7422623205e99729ea7f07d0144b354
Alexander G. Morano <amorano@gmail.com>
2025-07-19 19:40:40 -0400

    stripped everything except font extension

--- image_saver/__init__.py
+++
@@ -1,8 +0,0 @@
-﻿from .endless_image_saver import EndlessNode_Imagesaver
-
-NODE_CLASS_MAPPINGS = {
-    "Image_saver": EndlessNode_Imagesaver,
-}
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "Image_saver": "Image Saver",
-}
